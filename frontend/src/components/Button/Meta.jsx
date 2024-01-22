37f5be9f2938cdef7373a2868c0cf08627927805
Naveen <sce22cs026@sairamtap.edu.in>
2024-01-22 22:29:38 +0530

    init

--- frontend/src/components/Button/Meta.jsx
+++ frontend/src/components/Button/Meta.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-// import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import coinbase from "../../../public/coinbase.png";
+import MetaMask from "../../../public/MetaMask.png";
+import polygon from "../../../public/polygon.png";
 
 export const Meta = () => {
   return (
     <div classNAme="container">
       <div className="chooseYourWallet">
         <h4>Connect</h4>
-        {/* <FontAwesomeIcon icon={faTimes} /> */}
+        <FontAwesomeIcon icon={faTimes} />
       </div>
       <div className="options">
-        <img src="" alt="metamask" />
-        <img src="" alt="polygon" />
-        <img src="" alt="coinbase" />
-        <img src="" alt="rainbow" />
+        <img src="coinbase" alt="metamask" />
+        <img src="MetaMask" alt="polygon" />
+        <img src="polygon" alt="coinbase" />
       </div>
     </div>
   );
